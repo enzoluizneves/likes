2feb2e0ed3a264a922f8b6a69aa0da76887a188e
enzoluizneves <enzo.luiz.neves@escola.pr.gov.br>
2026-08-26 07:47:14 -0300

    Implement button click functionality to update counter

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const botoes = document.querySelectorall(".like");
 
+const contador = document.querySelector("#contador");
+
+botoes.forEach(function(botaoClicado){
+    botaoClicado("click", function(){
+        const valorSelecionado = Number(botaoClicado.dataset.valor);
+        contador.textContent = valorSelecionado;
+        botoes.forEach(function(botalAtual){
+            const valorAtual = Number(botaoAtual.dataset.valor);
+            if (valorAtual <= valorSelecionado){
+                botaoAtual.classList.add("ativo")
+            } else {
+                botaoAtual.classList.remove("ativo");
+            }
+        });
+    });
+});
